f6e234f819875db729e3c9c0ceb1d6cc3b9d5f3f
kk <kk@kk.com>
2024-12-12 11:26:06 +0800

    3

--- arch/src/main/java/com/zjy/architecture/ext/NetExts.kt
+++ arch/src/main/java/com/zjy/architecture/ext/NetExts.kt
@@ -1,14 +1,10 @@
 package com.zjy.architecture.ext
 
 import com.google.gson.Gson
-import com.zjy.architecture.Arch
-import me.jessyan.retrofiturlmanager.RetrofitUrlManager
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 import javax.net.ssl.SSLSocketFactory
 import javax.net.ssl.X509TrustManager
 
@@ -17,12 +13,6 @@ import javax.net.ssl.X509TrustManager
  * @since 2019/11/04
  * Description:创建[OkHttpClient]和[Retrofit]的方法
  */
-fun OkHttpClient.Builder.addChangeUrlInterceptor(manager: RetrofitUrlManager?): OkHttpClient.Builder {
-    if (manager == null) {
-        return this
-    }
-    return manager.with(this)
-}
 
 fun OkHttpClient.Builder.addInterceptors(vararg interceptor: Interceptor): OkHttpClient.Builder {
     interceptor.forEach {
@@ -51,27 +41,4 @@ fun createRetrofit(client: OkHttpClient, gson: Gson, url: String): Retrofit {
         .addConverterFactory(GsonConverterFactory.create(gson))
         .baseUrl(url)
         .build()
-}
-
-/**
- * 创建Retrofit对象通用方法
- *
- * @param manager       用于接口地址切换的对象
- * @param interceptor   具体业务逻辑所需的拦截器
- */
-fun createOkHttpClient(manager: RetrofitUrlManager?, sslFactory: SSLSocketFactory?,
-                       trustManager: X509TrustManager?, vararg interceptor: Interceptor): OkHttpClient {
-    val builder = OkHttpClient.Builder()
-        .connectTimeout(20L, TimeUnit.SECONDS)
-        .readTimeout(20L, TimeUnit.SECONDS)
-        .writeTimeout(20L, TimeUnit.SECONDS)
-        .addSSLSocketFactory(sslFactory, trustManager)
-        .addInterceptors(*interceptor)
-        .addChangeUrlInterceptor(manager)
-    if (Arch.debug) {
-        val loggingInterceptor = HttpLoggingInterceptor()
-        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
-        builder.addInterceptor(loggingInterceptor)
-    }
-    return builder.build()
 }
